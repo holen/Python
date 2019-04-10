77647916efdb7a85158c5b49b954bfaedbfa99fb
823772686@qq.com <823772686@qq.com>
2019-04-10 18:35:36 +0800

    git ignore

---
+++ design_pattern/test__new__init.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+class Person(object):
+    """Silly Person"""
+
+    def __new__(cls, name, age):
+        print('__new__ called.')
+        return super(Person, cls).__new__(cls, name, age)
+
+    def __init__(self, name, age):
+        print('__init__ called.')
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return '<Person: {}({})>'.format(self.name, self.age)
+
+
+if __name__ == '__main__':
+    piglei = Person('piglei', 24)
+    print(piglei)
+
