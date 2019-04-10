d89fd94d55339a7b6ee4d20cb07dcb0ba800f19c
823772686@qq.com <823772686@qq.com>
2019-04-10 18:38:22 +0800

    git ignore

--- design_pattern/test__new__init.py
+++ design_pattern/test__new__init.py
@@ -3,10 +3,9 @@
 
 class Person(object):
     """Silly Person"""
-
     def __new__(cls, name, age):
-        print('__new__ called.')
-        return super(Person, cls).__new__(cls, name, age)
+        print("in __new__")
+        return object.__new__(cls)  # don't pass extra arguments here!
 
     def __init__(self, name, age):
         print('__init__ called.')
@@ -14,7 +13,7 @@ class Person(object):
         self.age = age
 
     def __str__(self):
-        return '<Person: {}({})>'.format(self.name, self.age)
+        return 'Person: {}({})'.format(self.name, self.age)
 
 
 if __name__ == '__main__':
